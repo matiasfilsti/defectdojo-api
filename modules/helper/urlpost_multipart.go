e57be618392606a8c1c18f7887af6090030d4e6d
Matias Filsti <matiasfilsti@gmail.com>
2024-08-18 19:55:17 -0300

    cleaning code, separation of functions

--- modules/helper/urlpost_multipart.go
+++ modules/helper/urlpost_multipart.go
@@ -3,7 +3,6 @@ package helper
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -18,35 +17,13 @@ func MultipartPost(dojoUrl string, token string, configFile string, reportFile s
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
 	reportLoad(reportFile, writer)
-
-	config, err := os.Open(reportFile)
-	if err != nil {
-		return "", fmt.Errorf("error ocurred: %s", err.Error())
-
-	}
-	defer config.Close()
-
-	part, err := writer.CreateFormFile("file", filepath.Base(reportFile))
-	if err != nil {
-		return "", fmt.Errorf("error ocurred: %s", err.Error())
-	}
-
-	_, err = io.Copy(part, config)
-	if err != nil {
-		return "", fmt.Errorf("error ocurred: %s", err.Error())
-	}
-
-	/////
-	writer.WriteField("product_name", "Golang-Test")
-	writer.WriteField("engagement_name", "Tests1")
-	writer.WriteField("active", "true")
-	writer.WriteField("verified", "true")
-	writer.WriteField("scan_type", "Trivy Scan")
-
+	configLoad(configFile, writer)
 	writer.Close()
+	postUrl(dojoUrl, token, body, writer)
+	return "succeded report post", nil
+}
 
-	//////
-	fmt.Println(body)
+func postUrl(dojoUrl string, token string, body *bytes.Buffer, writer *multipart.Writer) (string, error) {
 	request, err := http.NewRequest("POST", dojoUrl, body)
 	if err != nil {
 		return "", fmt.Errorf("%v", err)
@@ -56,10 +33,7 @@ func MultipartPost(dojoUrl string, token string, configFile string, reportFile s
 	request.Header.Add("Content-Type", "multipart/form-data; boundary="+writer.Boundary())
 	request.Header.Add("Authorization", tokenValue)
 	request.ContentLength = int64(body.Len())
-	fmt.Println(tokenValue)
 	httpClient := &http.Client{}
-
-	/////
 	resp, err := httpClient.Do(request)
 	if err != nil {
 		return "", fmt.Errorf("%v", err)
@@ -79,48 +53,63 @@ func MultipartPost(dojoUrl string, token string, configFile string, reportFile s
 		return "", fmt.Errorf("%v", err)
 	}
 
-	if resp.StatusCode >= 400 && resp.StatusCode <= 500 {
-		return string(responseBody), errors.New("400/500 status code error")
-	}
-
 	return string(responseBody), nil
+
 }
 
-func reportLoad(reportFile string, writer *multipart.Writer) (string, *multipart.Writer, error) {
+func MultipartTest(dojoUrl string, token string, configFile string, reportFile string) (string, error) {
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	reportLoad(reportFile, writer)
+	configLoad(configFile, writer)
+
+	writer.Close()
+
+	//////
+	fmt.Println(body)
+	return "", nil
+}
+
+func reportLoad(reportFile string, writer *multipart.Writer) (*multipart.Writer, error) {
 
 	report, err := os.Open(reportFile)
 	if err != nil {
-		return "", nil, fmt.Errorf("error ocurred: %s", err.Error())
+		return nil, fmt.Errorf("error ocurred: %s", err.Error())
 
 	}
 	defer report.Close()
 
 	part, err := writer.CreateFormFile("file", filepath.Base(reportFile))
 	if err != nil {
-		return "", nil, fmt.Errorf("error ocurred: %s", err.Error())
+		return nil, fmt.Errorf("error ocurred: %s", err.Error())
 	}
 
 	_, err = io.Copy(part, report)
 	if err != nil {
-		return "", nil, fmt.Errorf("error ocurred: %s", err.Error())
+		return nil, fmt.Errorf("error ocurred: %s", err.Error())
 	}
 
-	return "", writer, nil
+	return writer, nil
 
 }
 
-func ConfigLoad(configFile string) {
+// func configLoad(configFile string, writer *multipart.Writer) (map[string]interface{}, error) {
+func configLoad(configFile string, writer *multipart.Writer) (*multipart.Writer, error) {
 	config, err := os.ReadFile(configFile)
 	if err != nil {
-		fmt.Println("error unmarshal file")
+		return nil, fmt.Errorf("error ocurred: %s", err.Error())
 	}
-	var dat map[string]interface{}
-	if err := json.Unmarshal(config, &dat); err != nil {
-		panic(err)
+	var configData map[string]interface{}
+	if err := json.Unmarshal(config, &configData); err != nil {
+		return nil, fmt.Errorf("error ocurred: %s", err.Error())
 	}
-	fmt.Println(dat)
-	for key, val := range dat {
-		fmt.Printf("key: %s, value: %v \n", key, val)
+	// for key, val := range dat {
+	// 	fmt.Printf("key: %s, value: %v \n", key, val)
+	// }
+	for key, val := range configData {
+		// fmt.Printf("key: %s, value: %v \n", key, val)
+		writer.WriteField(key, fmt.Sprint(val))
 	}
-	//return string(dat), nil
+	return writer, nil
 }
--- src/main.go
+++ src/main.go
@@ -21,7 +21,14 @@ func main() {
 	configFile := os.Args[3]
 	reportFile := os.Args[4]
 	fmt.Printf("%s, %s, %s, %s", dojoUrl, token, configFile, reportFile)
-	helper.ConfigLoad(configFile)
+	post, err := helper.MultipartPost(dojoUrl, token, configFile, reportFile)
+	if err != nil {
+		fmt.Errorf("error ocurred: %s", err.Error())
+
+	}
+	fmt.Println(post)
+	//helper.ConfigLoad(configFile)
+
 	// a, err := helper.MultipartPost3("http://localhost:8080/api/v2/import-scan/", "9bd07287e71bd2fc90e0fd7e1d9b78ac5ae12df8", "reports/trivy_report.json")
 	// if err != nil {
 	// 	fmt.Println(err)
